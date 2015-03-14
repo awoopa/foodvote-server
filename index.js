64792df9f185e3ff0a126d069b2ed42fadfee310
Jonathan Lui <kinwa91@msn.com>
2015-03-13 22:09:36 -0700

    added socket.io

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
